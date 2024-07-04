7ff4e7677909b8239acaa091d913faa4c3d83b4a
Kim O'Dell <kimbo.odell@gmail.com>
2024-07-03 21:31:37 -0400

    Add validation checks for user, add error messages for invalid registration or login info

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    
+
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
@@ -13,6 +13,7 @@ class SessionsController < ApplicationController
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
+      flash[:error] = "Invalid email or password"
       redirect_to '/login'
     end  
   end
