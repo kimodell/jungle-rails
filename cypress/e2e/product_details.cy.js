57e2dbeea095e5f2fed33370c84d99a3cd7a92d0
Kim O'Dell <kimbo.odell@gmail.com>
2024-07-17 20:13:12 -0400

    Modify test discription, correct spelling

--- cypress/e2e/product_details.cy.js
+++ cypress/e2e/product_details.cy.js
@@ -1,7 +1,7 @@
-describe('Product navigation and naviagates to product page', () => {
+describe('Visits home page and navigates to product page', () => {
   it('Visits the home page', () => {
     cy.visit('http://localhost:3000'); 
-    //products should be visiable
+    //products should be visable
     cy.get(".products article").should("be.visible");
     //click on article for giant tea
     cy.get('article').contains("Giant Tea").click();
