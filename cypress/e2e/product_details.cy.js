7e775e84f334a63db118e59fcf776ef98098cfdd
Kim O'Dell <kimbo.odell@gmail.com>
2024-07-17 20:37:54 -0400

    Modify test discriptions

--- cypress/e2e/product_details.cy.js
+++ cypress/e2e/product_details.cy.js
@@ -1,5 +1,5 @@
-describe('Visits home page and navigates to product page', () => {
-  it('Visits the home page', () => {
+describe('Product Navigation', () => {
+  it('Visits the home page, click a product and naviagates to product page', () => {
     cy.visit('http://localhost:3000'); 
     //products should be visable
     cy.get(".products article").should("be.visible");
